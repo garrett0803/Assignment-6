1739cf5f4eb9cb934c2825285a80130068cf53a3
garrett0803 <42852431+garrett0803@users.noreply.github.com>
2018-11-20 10:26:03 -0600

    v1.2
    ratio of fib only prints 1

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -2,18 +2,20 @@
 using namespace std;
 void fib(int fibo[20]);
 void fibprint(int fibo[20]);
+void fibrat(int fib[20]);
 int main()
 {
 int fibo[20];
 fib(fibo);
 fibprint(fibo);
+fibrat(fibo);
 return 0;
 }
 void fib(int fibo[20])
 {
    int i;
    fibo[0]=fibo[1]=1;
-  
+
   for(i=2;i<20;i++)
     fibo[i]=fibo[i-1]+fibo[i-2];
 }
@@ -23,7 +25,16 @@ void fibprint(int fibo[20])
     int i;
     cout.setf(ios::fixed);
     cout.setf(ios::showpoint);
-    cout.precision(8);	
+    cout.precision(8);
      for(i=2;i<20;i++)
         cout<<"\n"<<fibo[i];
+}
+void fibrat(int fibo[20])
+{
+  int i;
+  cout.setf(ios::fixed);
+  cout.setf(ios::showpoint);
+  cout.precision(8);
+  for(i=2;i<20;i++)
+  	 cout<<"\n"<<fibo[i]/fibo[i-1];
 }
